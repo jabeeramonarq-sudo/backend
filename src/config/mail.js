769455ce998a80f30da8750d9f2bd89574cf615e
sakshidesai15 <sakshidesai314@gmail.com>
2026-03-04 17:41:51 +0530

    Use EMAIL_PASSWORD for reply emails

--- src/config/mail.js
+++ src/config/mail.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
     secure: process.env.EMAIL_PORT == 465, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        pass: process.env.EMAIL_PASSWORD || process.env.EMAIL_PASS,
     },
 });
 
